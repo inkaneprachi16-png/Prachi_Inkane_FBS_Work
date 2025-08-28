cc4b2dedcfdbac90669ddbbd17762f00f2366d93
inkaneprachi16-png <inkaneprachi16@gmail.com>
2025-08-28 15:08:59 +0530

    Update ElectricUnitCharges.py

--- Assignment 3/ElectricUnitCharges.py
+++ Assignment 3/ElectricUnitCharges.py
@@ -3,4 +3,13 @@ unit = float(input('enter the electricity unit charges:'))
 if(unit <50):                       #for first 50 unit
     bill = unit* 0.50
 elif(unit<150):                     #for next 100 unit(50+100 = 150)
-    bill = 
+    bill = (50 * 0.50) + (unit - 100) *0.75
+elif(unit<250):
+    bill = (50 * 0.50)+(100 *0.75)+(unit-100)*1.20
+else:
+    bill = (50*0.50)+(100 * 0.75)+(100*1.20)+(unit-250)*1.50
+
+surcharge = bill * 2/100
+total_bill = bill +surcharge
+
+print(f' electric bill include 20% is {total_bill}')
