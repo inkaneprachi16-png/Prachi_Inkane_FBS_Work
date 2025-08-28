d7f97e5a5d53c96a095152b0eb4ea59b348d522a
inkaneprachi16-png <inkaneprachi16@gmail.com>
2025-08-28 22:12:17 +0530

    Update checkAlphabateVoweOrConsonent.py

--- Assignment 3/checkAlphabateVoweOrConsonent.py
+++ Assignment 3/checkAlphabateVoweOrConsonent.py
@@ -1,6 +1,7 @@
-a = input('enter aphabate to check the alphabet is vowel or consol:')
+a = input('enter aphabate to check the alphabet is vowel or consonant:')
 
 if(a in 'aeiouAEIOU'):
     print(f'{a} is an vowel')
 else:
-    print(f'{a} is an console')
+
+    print(f'{a} is an consonant')
