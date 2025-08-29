953734a1fd1e3b0bc985b01aecb1eb238d27d624
inkaneprachi16-png <inkaneprachi16@gmail.com>
2025-08-29 10:28:18 +0530

    Delete Assignment 2 directory

--- Assignment 2/CelciusToFharenhit.py
+++
@@ -1,5 +0,0 @@
-c = float(input('Enter the temperature: '))
-
-f = ( c * 9 / 5 ) + 32
-
-print(f'The given temperature in celcius is converted into Fahrenheit is {f} f')
--- Assignment 2/reversedDigit.py
+++
@@ -1,7 +0,0 @@
-n = int(input('enter a number:'))
-hundred = n // 100
-ten = (n // 10) % 10
-unit = n % 10
-
-
-print(f'reverse of the given three digits {n} is {unit}{ten}{hundred}')
