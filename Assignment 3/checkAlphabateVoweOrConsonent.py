f81c556444a090c41decf46ef56643ed0a63ba4e
inkaneprachi16-png <inkaneprachi16@gmail.com>
2025-08-29 10:28:38 +0530

    Delete Assignment 3 directory

--- Assignment 3/checkAlphabateVoweOrConsonent.py
+++
@@ -1,7 +0,0 @@
-a = input('enter aphabate to check the alphabet is vowel or consonant:')
-
-if(a in 'aeiouAEIOU'):
-    print(f'{a} is an vowel')
-else:
-
-    print(f'{a} is an consonant')
--- Assignment 3/positiveNegativeNum.py
+++
@@ -1,6 +0,0 @@
-num = int(input('enter a number:'))
-
-if(num > 0):
-    print(f'{num} is positive number')
-else:
-    print(f'{num} is negative number')
