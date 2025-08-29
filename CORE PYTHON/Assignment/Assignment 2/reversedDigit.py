3d18fa1e456bcc015a7a81d314617f7020ed970f
inkaneprachi16-png <inkaneprachi16@gmail.com>
2025-08-29 14:57:41 +0530

    Update reversedDigit.py

--- CORE PYTHON/Assignment/Assignment 2/reversedDigit.py
+++ CORE PYTHON/Assignment/Assignment 2/reversedDigit.py
@@ -1,12 +1,18 @@
-n = int(input('enter a 3 digit number:'))
-
-#seprate digits
-hundred = n // 100
-ten = (n // 10) % 10
-unit = n % 10
+num = int(input('enter a 3 digit number:'))
 
 
-print(f'reverse of the given three digits {n} is {unit}{ten}{hundred}')
+#digit spration
+d1 = num % 10
+num = num //10
+
+d2 = num % 10
+num = num // 10
+
+d3 = num % 10
+num = num // 10
+
+
+print(f'reverse of the given three digits  is {d1} {d2} {d3}')
 
 
 
