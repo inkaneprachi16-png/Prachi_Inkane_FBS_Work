26d0ccf69cd45a9114d06d489aeed2162e1ef9f0
inkaneprachi16-png <inkaneprachi16@gmail.com>
2025-08-29 10:28:53 +0530

    Delete Test 1 directory

--- Test 1/Que2.py
+++
@@ -1,7 +0,0 @@
-p= int(input('enter the principle amount:'))
-r= int(input('enter the rate of interest:'))
-t= int(input('enter the time period:'))
-
-si = p*r*t/100
-
-print(f'The simple interest is {si}')
