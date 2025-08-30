11a56065002092a4e0e60c1ca2a53bf7fef94393
inkaneprachi16-png <inkaneprachi16@gmail.com>
2025-08-30 18:34:40 +0530

    Update QuadraticEque.py

--- CORE PYTHON/Assignment/Assignment 1/QuadraticEque.py
+++ CORE PYTHON/Assignment/Assignment 1/QuadraticEque.py
@@ -1,4 +1,4 @@
-from math import *
+
 a = int(input('Enter the coefficient a:'))
 b = int(input('Enter the coefficient b:'))
 c = int(input('Enter the coefficient c:'))
@@ -7,6 +7,7 @@ x1 = - b + (( b ** 2 - 4 * a * c)**0.5 )/ 2*a
 x2 = - b - (( b ** 2 - 4 * a * c)**0.5) / 2*a
 
 print(f'roots of Quadratic equation is x1 = {x1} and x2 = {x2}')
+
 
 
 # a=1 b=5 c=6      x=-2, x=-3
