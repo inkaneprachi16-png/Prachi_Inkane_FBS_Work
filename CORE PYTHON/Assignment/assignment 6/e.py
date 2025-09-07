c6c0075de11c9ee1d2b587a0ad9938837d70af93
inkaneprachi16-png <inkaneprachi16@gmail.com>
2025-09-07 22:15:04 +0530

    Update e.py

--- CORE PYTHON/Assignment/assignment 6/e.py
+++ CORE PYTHON/Assignment/assignment 6/e.py
@@ -1,7 +1,8 @@
 for i in range(1,6):
-    for j in range(1, 10-i):
+    for j in range(1, 7-i):
         print('   ', end=' ')
 
     for j in range(2* i-1):
         print('*', end= '   ')
+
     print()
