21a4e1453c8dcfe78ce22f4de1f0258757a18e3b
inkaneprachi16-png <inkaneprachi16@gmail.com>
2025-09-07 22:15:38 +0530

    Update f.py

--- CORE PYTHON/Assignment/assignment 6/f.py
+++ CORE PYTHON/Assignment/assignment 6/f.py
@@ -1,9 +1,10 @@
 number = 1
 for i in range(1, 6):
-    for j in range(1, 10-i):
+    for j in range(1, 7-i):
         print(' ', end =' ')
 
     for j in range(1, 2*i):
         print(j, end=' ')
     number +=1
+
     print()
