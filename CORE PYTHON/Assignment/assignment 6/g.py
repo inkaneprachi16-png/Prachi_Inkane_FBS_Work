a205e8012f70b37c4525d9754ab5f581ad206933
inkaneprachi16-png <inkaneprachi16@gmail.com>
2025-09-07 22:16:22 +0530

    Update g.py

--- CORE PYTHON/Assignment/assignment 6/g.py
+++ CORE PYTHON/Assignment/assignment 6/g.py
@@ -1,9 +1,10 @@
 
 for i in range(1,6):
-    for j in range(1, 10-i):
+    for j in range(1, 7-i):
         print('  ', end = '  ')
     
     for j in range(2 * i -1):
         print(chr(65+j) , end ='   ')
     print()
+
     
