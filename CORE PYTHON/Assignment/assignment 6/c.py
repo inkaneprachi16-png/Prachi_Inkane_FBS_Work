59ae7f7adfd2a5b658bd8e41bc3f4bf5f48ce657
inkaneprachi16-png <inkaneprachi16@gmail.com>
2025-09-09 16:42:02 +0530

    Update c.py

--- CORE PYTHON/Assignment/assignment 6/c.py
+++ CORE PYTHON/Assignment/assignment 6/c.py
@@ -1,7 +1,15 @@
-for i in range(1,5):
-    for j in range(1, 0, -1):
-        print(j, end = ' ')
+n= int(input('enter number:'))
+for i in range(n):
+    for j in range(1, n-i):
+        print('', end =' ')
+    num=1
+    for j in range(i+1):
+        print(num,end =' ')
+        num = num*(i-j)//(j+1)
+
     print()
 
 
-    #Pending
+
+
+    
