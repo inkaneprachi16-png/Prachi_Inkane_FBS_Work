4f1dc8aa0915164095e422b13a7570bb9d773881
inkaneprachi16-png <inkaneprachi16@gmail.com>
2025-09-21 10:35:18 +0530

    Update keyExitDictionary.py

--- CORE PYTHON/Assignment/Assignment 13/keyExitDictionary.py
+++ CORE PYTHON/Assignment/Assignment 13/keyExitDictionary.py
@@ -2,6 +2,7 @@ di1 = {'name':'Prachi', 'age':24, 'sal':45000, 'add':'pune'}
 key = input('enter key name:')
 
 if key in di1:
-    print('the key exit in dictionry')
+    print('the key exist in dictionry')
 else:
-    print('the key  not exit in dictionry')
+
+    print('the key  not exist in dictionry')
